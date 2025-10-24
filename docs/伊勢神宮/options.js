9176cfd8fc4d295bbcbc062ce762fcf162511fc3
gp23a075-spec <gp23a075@oecu.jp>
2025-10-24 16:57:59 +0900

    Add files via upload

---
+++ docs/伊勢神宮/options.js
@@ -0,0 +1,85 @@
+let search_cond = 
+[
+  {
+    "id": "cond1",
+    "ctext": "分類=神宮",
+    "cond": "wdt:P31",
+    "val": "wd:Q120680118",
+    "type": "ID",
+    "const": true
+  },
+  {
+    "id": "cond2",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond3",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond4",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond5",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  },
+  {
+    "id": "cond6",
+    "ctext": "",
+    "cond": "",
+    "val": "",
+    "type": "ID",
+    "const": false
+  }
+];
+
+let search_prop = 
+[
+  {
+    "id": "opt1",
+    "prop": "wdt:P131",
+    "pname": "位置する行政区画",
+    "optional": true
+  },
+  {
+    "id": "opt2",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt3",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt4",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  },
+  {
+    "id": "opt5",
+    "prop": "",
+    "pname": "",
+    "optional": true
+  }
+];
